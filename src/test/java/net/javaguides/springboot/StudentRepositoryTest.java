d0cf58f289de8a7314fe07d15d0d764ed793ee11
Anuj khare <anujkhare92@gmail.com>
2023-09-16 16:09:29 +0530

    .

--- src/test/java/net/javaguides/springboot/StudentRepositoryTest.java
+++ src/test/java/net/javaguides/springboot/StudentRepositoryTest.java
@@ -3,7 +3,6 @@ package net.javaguides.springboot;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
--- src/test/java/net/javaguides/springboot/StudentServiceTest.java
+++ src/test/java/net/javaguides/springboot/StudentServiceTest.java
@@ -1,5 +1,6 @@
 package net.javaguides.springboot;
 
+import net.javaguides.springboot.exception.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,9 +8,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,6 +35,7 @@ public class StudentServiceTest {
 	@BeforeEach
 	public void setup() {
 		stud = new Student();
+		stud.setId(0);
 		stud.setFirstName("Anuj");
 		stud.setLastName("Khare");
 		stud.setEmail("anuj@gmail.com");
@@ -40,17 +47,23 @@ public class StudentServiceTest {
 		//when
 		Student std1 = studService.saveStudent(stud);
 		//then
+		assertThat(std1).isEqualTo(stud);
 		assertThat(std1).isNotNull();
 	}
 	@Test
 	public void getStudentByIdTest() {
 		//given
-		
 		//when
 		given(studRepo.findById(stud.getId())).willReturn(Optional.of(stud));
 		//then
 		Student savedStudent = studService.getStudentById(stud.getId());
 		assertThat(savedStudent).isEqualTo(stud);
+	}
+	@Test
+	public void getStudentByInvalidIdTest() {
+		given(studRepo.findById(2L)).willReturn(Optional.empty());
+		assertThrows(ResourceNotFoundException.class,
+				()-> studService.getStudentById(2L));
 	}
 	@Test 
 	public void getAllStudentsTest(){
@@ -66,6 +79,30 @@ public class StudentServiceTest {
 		//then
 		assertThat(studList).isNotNull();
 		assertThat(studList.size()).isEqualTo(2);
+	}
+	@Test
+	public void getAllStudentsNoStudentsPresentTest(){
+		given(studRepo.findAll()).willReturn(new ArrayList<>());
+		List<Student> studList = studService.getAllStudents();
+		assertThat(studList).isEmpty();
+	}
+	@Test
+	public void deleteStudentValidIdTest() {
+		//given
+		given(studRepo.findById(0L)).willReturn(Optional.of(stud));
+		willDoNothing().given(studRepo).delete(stud);
+		//when
+		studService.DeleteStudent(stud.getId());
+		verify(studRepo,times(1)).findById(0L);
+		//then
+		verify(studRepo,times(1)).delete(stud);
+	}
+	@Test
+	public void deleteStudentInValidIdTest() {
+		given(studRepo.findById(1L)).willReturn(Optional.empty());
+		assertThrows(ResourceNotFoundException.class,()->studService.DeleteStudent(1L));
+		verify(studRepo,times(1)).findById(1L);
+		verify(studRepo,times(0)).delete(stud);
 	}
 	
 }
